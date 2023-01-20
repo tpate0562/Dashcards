65b7630e94bb121746c1be7f222b0c14effb693d
tpate0562 <tpate0562@stu.wusd.org>
2023-01-20 13:14:12 -0800

    FIXED BUTTONS

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ terms["aegis", "antediluvian", "apex", "avuncular", "boondoggle", "cataclysmic",
 definitions["protection; guidance; support", "literally means before the flood; old-fashioned; outdated", "the highest point; to reach a high point", "uncle-like; friendly and chummy", "an activity/project that is a waste of time/money--but appears worthwhile", "destructive; ruinous(debacle)", "swiftness; quickness of movement (adroit)", "one's power or influence; cachet, gravitas", "a scarcity; a lack of (antiprodigious, superfluous)", "to disperse or scatter; to disappear", "to weaken; to feel drained of energy", "of or due from a son or daughter", "an embarrassing remark/behavior in a social situation--literally a false step", "holding beliefs at odds with the majority", "relaxed and at ease; imprecise and unstructured", "a countless or extremely great number (prodigious)", "generous, magnanimous (parsimonious)" ,"excessively anxious, tense, high strung" ,"noisy and difficult to control; unruly", "a scarcity, a lack of, dearth", "common; low class; lacking refinement", "skill and expertise in a particular field; adroit", "idealistic; impractical; starry-eyed", "formidable; fearsome; deserving of respect", "stubborn; uncooperative; obstinate", "a lengthy, complicated procedure--one that becomes tiresome", "expressing sorrow or regret; remorseful", "juvenile, immature; foolish", "dependable and loyal; sturdy and strong", "calm; showing little or no emotion; stoic", "uncommunicative in speech; saying little; tight-lipped", "a private conversation between two people--literally head to head", "ashionable; trendy; swanky", "resentment/offense (idiom: to take ... at)", "being truthful; honest", "cruel; bitter, angry, acrimonious"];
 
 function displayQuestion() {
-    document.getElementById("btn1").innerHTML = "hello";
-    document.getElementById("btn2").innerHTML = terms[1];
+    document.getElementById("btn1").value = "hello";
+    document.getElementById("btn2").value = terms[1];
 }
 function clickButtonOne() {
     optionOne = true;
