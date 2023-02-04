ad1a135c64809c9be0a10f1dd2944bc07e108018
tpate0562 <tpate0562@stu.wusd.org>
2023-02-03 17:47:16 -0800

    Lots of stuff changed and a few things broke

--- createdeck.js
+++ createdeck.js
@@ -1,5 +1,10 @@
-function download(setTerms, setName) {
+function downloadDeck(setTerms, setName) {
     var a = document.createElement('a');
     a.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(setTerms));
     a.setAttribute('download', setName);
+
+    a.style.display = 'none';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
 }
