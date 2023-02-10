88e33026d417e8b0178de0c2ed559b89ac5f1c46
Aquarmity <62366478+Aquarmity@users.noreply.github.com>
2023-02-09 21:11:35 -0800

    Added to createdeck.js
    Not done yet, but added a lot of new features. Can create additional text fields, can set a title for download, parsing system is setup, and some other stuff

--- createdeck.js
+++ createdeck.js
@@ -1,10 +1,39 @@
-function downloadDeck(setTerms, setName) {
+termsAndDefs = [];
+
+function downloadDeck() {
+    //TODO: change 5
+    for (let i = 0; i < 5; i++) {
+        termsAndDefs[i] = document.getElementById("term1").value;
+        if (termsAndDefs[i].includes(',')) {
+            termsAndDefs[i] = termsAndDefs[i].replace(/,/g, "\\"); //I'm going to regret having double backslash aren't I
+        }
+        if (termsAndDefs[i].includes('\\')) {
+            termsAndDefs[i] = termsAndDefs[i].replace(/\\/g, "\\\\");
+        }
+    }
+
     var a = document.createElement('a');
-    a.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(setTerms));
-    a.setAttribute('download', setName);
+    a.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(termsAndDefs));
+    a.setAttribute('download', document.getElementById("title").value + ".txt");
 
     a.style.display = 'none';
     document.body.appendChild(a);  
     a.click();
     document.body.removeChild(a);
+
+}
+
+//TODO: create id for each new input with new number. maybe animate?? that'd be dumb
+function addTermInput() {
+    let newTerm = document.createElement("input");
+    newTerm.setAttribute("type", "text");
+    newTerm.setAttribute("placeholder", "Term");
+
+    let newDef = document.createElement("input");
+    newDef.setAttribute("type", "text");
+    newDef.setAttribute("placeholder", "Definition");
+
+    document.getElementById("deck-input").appendChild(newTerm);
+    document.getElementById("deck-input").appendChild(newDef);
+    document.getElementById("deck-input").appendChild(document.createElement("br"));
 }
