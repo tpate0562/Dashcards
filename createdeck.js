716d9c28f0d9eb297b5715ee34a73b5bd7ebad5e
Aquarmity <62366478+Aquarmity@users.noreply.github.com>
2023-02-10 19:10:57 -0800

    downloadDeck() and addTermInput() finished
    (maybe)
    Also, please note that createdeck.html is *not* done, there's major style issues currently

--- createdeck.js
+++ createdeck.js
@@ -1,14 +1,16 @@
 termsAndDefs = [];
 
+//this variable name is somewhat misleading. It's total terms + definitions, because each def is counted as a term
+let totalTerms = 3;
+
 function downloadDeck() {
-    //TODO: change 5
-    for (let i = 0; i < 5; i++) {
-        termsAndDefs[i] = document.getElementById("term1").value;
-        if (termsAndDefs[i].includes(',')) {
-            termsAndDefs[i] = termsAndDefs[i].replace(/,/g, "\\"); //I'm going to regret having double backslash aren't I
-        }
+    for (let i = 0; i < totalTerms - 1; i++) {
+        termsAndDefs[i] = document.getElementById("input" + (i + 1)).value;
         if (termsAndDefs[i].includes('\\')) {
             termsAndDefs[i] = termsAndDefs[i].replace(/\\/g, "\\\\");
+        }
+        if (termsAndDefs[i].includes(',')) {
+            termsAndDefs[i] = termsAndDefs[i].replace(/,/g, "\\");
         }
     }
 
@@ -28,12 +30,16 @@ function addTermInput() {
     let newTerm = document.createElement("input");
     newTerm.setAttribute("type", "text");
     newTerm.setAttribute("placeholder", "Term");
+    newTerm.setAttribute("id", "input" + totalTerms);
 
     let newDef = document.createElement("input");
     newDef.setAttribute("type", "text");
     newDef.setAttribute("placeholder", "Definition");
+    newDef.setAttribute("id", "input" + (totalTerms + 1));
 
     document.getElementById("deck-input").appendChild(newTerm);
     document.getElementById("deck-input").appendChild(newDef);
     document.getElementById("deck-input").appendChild(document.createElement("br"));
+
+    totalTerms += 2;
 }
